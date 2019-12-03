6d7172bede9b50be34dfe0370b9dd07ce67711af
Simon Lee <simon.lee@cardinalblue.com>
2019-12-03 11:05:02 +0800

    Simplified printing solutions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod day1;
 mod day2;
 
 fn main() {
-    println!("Day 1: ({}, {})", day1::solve().0, day1::solve().1);
+    println!("Day 1: {:?}", day1::solve());
     println!("Day 2: {:?}", day2::solve());
 }
